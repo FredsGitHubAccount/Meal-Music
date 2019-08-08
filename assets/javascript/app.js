5f87d27229f3dfe42eab8d8bf8b7b784a3f2cb8a
Fred Saba <fred@fred-saba.com>
2019-08-07 22:57:37 -0700

    added additional requests for yelp and minor css changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,18 @@
-let shortcut = ""
-let queryURL = ""
-let queryParams = ""
+
+
 
 let apiKEY = "Bearer lhxtMMrJKWK0-jlsuWz-Nr_TLtpMP00yllbOkFIDCP3GE9RQpZ0i9DBLzwNhvI5jsRPTZcQzoKAgoXiQbk1RkN_nokEXjigC5mozh90EGcO6217_NOGZtSxpm_hIXXYx"
 
 
 function buildQueryUrl () {
 
-queryURL = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?`
-queryParams = {}
+let queryURL = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?`
+let queryParams = {}
 queryParams.term = "restaurants"
-queryParams.location = 'Berkeley'
+queryParams.location = 'berkeley'
 // queryParams.latitude = '37.871593' *WILL USE GOOGLE API FOR LOCATION*
 // queryParams.longitude ='-122.272743'*WILL USE GOOGLE API FOR LOCATION*
-queryParams.categories = $("#rest-input").val().trim()
+queryParams.categories= $("#rest-input").val().trim()
 queryParams.limit = 3
 return queryURL + $.param(queryParams) 
 
@@ -25,6 +24,7 @@ return queryURL + $.param(queryParams)
 $("#rest-search").on("click",function(event){
 
     $("#rest-holder").empty()
+   
     event.preventDefault()
     let completeURL = buildQueryUrl()
     console.log(buildQueryUrl())
@@ -37,6 +37,7 @@ $.ajax({
    
 }).then(function(response){
 
+    $("#rest-input").val("")
     shortcut = response.businesses
     
     for (let i = 0; i < shortcut.length; i++){
@@ -45,14 +46,20 @@ $.ajax({
 
         let imgURL = shortcut[i].image_url
 
-        let restDiv = $(`<div class="shadow p-3 mb-5 jumbotron bg-white rounded rest-image" style="margin:25px">`)
+        let restDiv = $(`<div class="shadow jumbotron bg-white rounded new-rest" style="margin:25px">`)
         let restTitle = $(`<h2>${shortcut[i].name}</h2>`)
-        restDiv.append(restTitle)
+        let restRating = $(`<h3>Rating : ${shortcut[i].rating} out of 5 stars!</h3>`)
         let restImg = $("<img>")
         restImg.attr("src", imgURL)
-        restImg.addClass("img-fluid rounded")
+        restImg.addClass("img-fluid rounded newImg")
+        let restAdress = $(`<h5>Address : ${shortcut[i].location.display_address}</h5>`)
+        let restWebsite = $(`<h5><a href=${shortcut[i].url}>${shortcut[i].name} Website</a></h5>`)
+
+        restDiv.append(restTitle)
+        restDiv.append(restRating)
         restDiv.append(restImg)
-    
+        restDiv.append(restAdress)
+        restDiv.append(restWebsite)
         $("#rest-holder").append(restDiv)
          
     }
